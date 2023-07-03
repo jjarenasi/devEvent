47680c6b762f4a86791ec80aac6dcd8d89478c9b
Juan Arenas <juan_arenas_iparraguirre@mckinsey.com>
2023-07-03 16:03:42 -0500

    config aws

--- src/main/java/com/example/testevent/controller/RequestEventController.java
+++ src/main/java/com/example/testevent/controller/RequestEventController.java
@@ -2,7 +2,7 @@ package com.example.testevent.controller;
 
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -18,15 +18,15 @@ import com.example.testevent.entity.EventTest;
 @RestController
 @RequestMapping("api/event")
 public class RequestEventController {
-    @Value("${api_key.name}")
-    private String api_key;
+    // @Value("${api_key.name}")
+    // private String api_key;
 
     @PostMapping("/request")
     public ResponseEntity<String> saveuser(@RequestBody String user){
         String uri = "https://b18hlwsdpi.execute-api.us-east-2.amazonaws.com/prod/sqs";
         HttpHeaders headers = new HttpHeaders();
-        //headers.add("x-api-key", "ndmgozALnU6l2n16YZrjta01wbXVn2oGQS0MWQe0");
-        headers.add("x-api-key", api_key);
+        headers.add("x-api-key", "ndmgozALnU6l2n16YZrjta01wbXVn2oGQS0MWQe0");
+        //headers.add("x-api-key", api_key);
         EventTest event = new EventTest(UUID.randomUUID().toString(), "mensaje");
         HttpEntity<EventTest> entity = new HttpEntity<EventTest>(event,headers); 
         RestTemplate restTemplate = new RestTemplate();
